f5d8617530f08cab648f6baf02cf7f1c033ad6a0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-03 11:52:26 +0200

    new code is added now

--- src/app/Components/Honeypages.jsx
+++ src/app/Components/Honeypages.jsx
@@ -3,9 +3,9 @@ import Oneimg from '../Image/pexels-fauxels-3184285.jpg';
 
 export default function Honeypages() {
     return (
-        <div className="relative min-h-screen w-full max-w-full bg-fixed"
+        <div className="relative min-h-screen w-full max-w-full bg-fixed object-fill"
             style={{
-                background: `linear-gradient(to right, rgba(4, 5, 5, 1.601), rgba(0, 0, 0, 0.112)), url(${Oneimg.src}) center / cover no-repeat fixed`,
+                background: `linear-gradient(to right, rgba(4, 5, 5, 0.801), rgba(0, 0, 0, 0.112)), url(${Oneimg.src}) center / cover no-repeat fixed`,
             }}
         >
         </div>
