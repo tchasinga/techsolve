742a1c27e2e3e163c43745987c7183d0a6db1a64
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-03 12:39:13 +0200

    new code is added now

--- src/app/Components/Navbars.jsx
+++ src/app/Components/Navbars.jsx
@@ -4,17 +4,17 @@ import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
 
 export default function Navbars() {
   return (
-    <div className="mx-auto max-w-5xl  border-2 py-3 flex justify-between items-center">
+    <div className="mx-auto max-w-5xl py-3 flex justify-between items-center">
         <div className="w-[150px]">
             <Image src={ComponentWhatsApp} />
         </div>
         <div className="">
-            <ul className="flex items-center justify-center gap-3">
-                <li>Introduction</li>
-                <li>About TSK</li>
-                <li>Our services</li>
-                <li>Teams</li>
-                <li>Developments</li>
+            <ul className="flex items-center flex-wrap text-sm font-medium justify-center gap-6">
+                <li className="hover:cursor-pointer hover:text-green-800 duration-500">Introduction</li>
+                <li className="hover:cursor-pointer hover:text-green-800 duration-500">About TSK</li>
+                <li className="hover:cursor-pointer hover:text-green-800 duration-500">Our services</li>
+                <li className="hover:cursor-pointer hover:text-green-800 duration-500">Teams</li>
+                <li className="hover:cursor-pointer hover:text-green-800 duration-500">Developments</li>
             </ul>
         </div>
     </div>
