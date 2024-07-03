29f7ee5bbf5f6ab4ebca3d8912eda0349f0ad4d1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-03 13:27:05 +0200

    new code is added now for really

--- src/app/Components/CorporateSoftware.jsx
+++ src/app/Components/CorporateSoftware.jsx
@@ -6,7 +6,15 @@ export default function CorporateSoftware() {
             <h1 className="uppercase text-white text-9xl font-bold border">we</h1>
             <h1 className="uppercase text-white text-8xl font-bold border">implement</h1>
             <h2 className="text-white uppercase text-5xl font-bold first-letter:uppercase border">corporate sowftware</h2>
-            <div className="border">
+          
+            <div className="border flex flex-col justify-center text-sm text-slate-400 first-letter:uppercase p-3">
+                <h1>We implement the corporate performance</h1>
+                <h1>Software at <span className="font-bold"> Techsol Software kenya </span> for consolidation planning</h1>
+                <h1>And reporting porspuses</h1>
+
+                <div className="bg-green-800 p-4 w-[15%]">
+                    <h1>Get Stared Now</h1>
+                </div>
             </div>
         </div>
       );
