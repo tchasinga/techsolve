31ef0704e1c7d245b0cd6ad20d2f19419cb11021
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-04 08:43:08 +0200

    adding some new file and push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Honeypages from "./Components/Honeypages";
 import Honepagestwo from "./Components/Honepagestwo";
+import Description from "./Components/Description";
 
 export default function Home() {
   return (
     <main className="max-w-full">
       <Honeypages />
       <Honepagestwo />
+      <Description />
     </main>
   );
 }
