dfbaadee16b1ef4b09d1c35d2ee6be43f1c0b75d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-04 09:00:52 +0200

    new code is added now

--- src/app/Components/Description.jsx
+++ src/app/Components/Description.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Oneimg from '../Image/pexels-thisisengineering-3861967.jpg';
 
 export default function Description() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="min-h-screen relative w-full max-w-full bg-fixed object-fill"
+            style={{
+                background: `linear-gradient(rgba(248, 248, 248, 0.926), rgba(248, 248, 248, 0.926)), url(${Oneimg.src}) center / cover no-repeat fixed`,
+            }}
+        >
+
+        </div>
+    )
 }
