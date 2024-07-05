15e5fed96b46596bbe62fcf7a1c7c8303cf4de51
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-05 10:59:12 +0200

    new code is added now

--- src/app/Components/Ourservice.jsx
+++ src/app/Components/Ourservice.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react'
 import Oneimg from '../Image/pexels-pixabay-269077.jpg'
 import myDataobject from '../Data/Dataone.js'
-import CenterMode from './CenterMode';
 
 
 export default function Ourservice() {
@@ -11,6 +10,11 @@ export default function Ourservice() {
     useEffect(() => {
         setClientData(myDataobject.slice(0, 6));
     }, []);
+
+    const [someClientData, someSetClientData] = useState([]);
+    useEffect(() =>{
+        someSetClientData(myDataobject.slice(6, 10));
+    })
 
 
 
@@ -43,7 +47,27 @@ export default function Ourservice() {
                 </div>
 
                 {/* Adding carousel code */}
-                <CenterMode />
+                <h1 className="text-4xl text-slate-100 font-bold py-5 uppercase">Our Provided Services at tsk</h1>
+                <div className="max-w-5xl mx-auto">
+                    <div className="gridGeneral">
+                        {someClientData.map((item) => (
+                            <div key={item.id} className="bg-white bg-opacity-50 p-4 py-4 rounded-lg">
+                                <div className="flex-col flex justify-center items-center">
+                                    <div className="bg-blue-400 p-5 rounded-full text-2xl my-3">
+                                        {item.MyIcons}
+                                    </div>
+                                    <div className="flex items-center justify-center mb-2">
+                                        <h1 className="text-lg font-bold">{item.myTitle}</h1>
+                                    </div>
+                                </div>
+                                <div className="line-clamp-4 text-slate-900">
+                                    <p>{item.Description}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
