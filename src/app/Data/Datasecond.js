815305b9f2a79eda1d565a3920dd6c4a5faf0681
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-05 12:33:42 +0200

    refactor: Add new data to Datasecond.js

--- src/app/Data/Datasecond.js
+++ src/app/Data/Datasecond.js
@@ -1,5 +1,34 @@
+import myImgOne from '../Design/AmIdisplayResponsive.png';
+import myImgTwo from '../Design/LearningNative.png'
+import myImgThree from '../Design/Screenshot.png'
+import myImgFour from '../Design/crops.png'
+
+
 const secondlydata = [
-    
+    {
+        id: 1,
+        ProjectTitle: "Program path",
+        ImagesSide: myImgOne,
+        myDescription: "The program path refers to the specific sequence of folders and directories that the software follows to execute a program or access resources. It outlines the hierarchical structure of files and directories necessary for the program to function correctly. Ensuring the correct program path is crucial for the seamless operation and execution of the software."
+    },
+    {
+        id: 2,
+        ProjectTitle: "React Native Learning",
+        ImagesSide: myImgTwo,
+        myDescription: "React Native Learning involves acquiring the skills and knowledge to build mobile applications using the React Native framework. It encompasses understanding JavaScript, React components, and mobile development principles. Mastering React Native enables developers to create cross-platform apps with native performance and a unified codebase."
+    },
+    {
+        id: 3,
+        ProjectTitle: "Landing Estate",
+        ImagesSide: myImgThree,
+        myDescription: "Landing Estate Seller specializes in the marketing and sale of residential and commercial properties. They offer comprehensive services, including property valuation, listing, and negotiation support. Their goal is to ensure a smooth and successful transaction for both buyers and sellers in the real estate market"
+    },
+    {
+        id : 4,
+        ProjectTitle: "Gymness room",
+        ImagesSide: myImgFour,
+        myDescription : "Gymness Room is a dedicated fitness space designed to accommodate various workout routines and exercises. It features equipment such as weights, machines, and cardio gear to support strength training, cardio workouts, and flexibility exercises. The goal of a Gymness Room is to provide a convenient and effective environment for individuals to achieve their fitness goals."
+    }
 ]
 
 
