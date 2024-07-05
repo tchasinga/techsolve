e581e04f4496ef5997ac5e4494f152df3a64dcbb
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-05 08:30:39 +0200

    new code is added now

--- src/app/Components/Providerservice.jsx
+++ src/app/Components/Providerservice.jsx
@@ -1,34 +1,56 @@
-"use client";
-import React, { useEffect, useState } from 'react';
+"use client"
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Providerservice() {
     const spanChanger = [
         "Website Development",
-        "App Development Android & IOS",
+        "App Android & IOS",
         "Web applications",
         "UI design",
-        "Digital Marketing and Branding",
+        "Digital Marketing & Branding",
         "Brand Design",
         "Professional Training",
-        "POS System - Software and Hardware",
+        "POS System - Software & Hardware",
         "SEO Services",
-        "Digital Marketing and Branding",
+        "Digital Marketing & Branding",
         "Photo editing",
     ];
 
+    const [index, setIndex] = React.useState(0);
+
+    React.useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % spanChanger.length);
+        }, 3000); // Change text every 3 seconds
+
+        return () => clearInterval(intervalId);
+    }, [spanChanger.length]);
 
     return (
-        <div className="h-[100px] bg-slate-200 flex justify-center items-center gap-5">
-            <div className="">
-                <h1 className="first-letter:uppercase">
+       <div className="bg-slate-200">
+            <div className="h-[100px] uppercase flex justify-center items-center gap-5">
+            <div>
+                <h1 className="text-4xl font-bold text-slate-900 ">
                     we provide a trusted service in
                 </h1>
             </div>
 
-            <div className="">
-                 <h1>{spanChanger}</h1>
+            <div>
+            <AnimatePresence mode="wait">
+                    <motion.h1
+                        className=" text-4xl text-gray-800 font-medium"
+                        key={index}
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 20 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        {spanChanger[index]}
+                    </motion.h1>
+                </AnimatePresence>
             </div>
-
         </div>
+       </div>
     );
 }
