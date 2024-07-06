b13e721d33a880cff8572b6453ba83360dd2bc6d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-06 09:32:41 +0200

    new code is added now

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
+import myIgm from '../Image/pexelsdivinetechygirl.jpg'
 
 export default function Footer() {
-  return (
-    <div className="min-h-[500px]"
-    
-    >
-        
-        <div className="max-w-screen-xl mx-auto">
-            
-            <div className="">
+    return (
+        <div className="min-h-[500px]"
+            style={{
+                background: `linear-gradient(rgba(4, 5, 5, 0.716), rgba(4, 5, 5, 0.716)), url(${myIgm.src}) center / cover no-repeat fixed`,
+            }}
+        >
+
+            <div className="max-w-screen-xl mx-auto">
+
+                <div className="">
+
+                </div>
+
+                <div className=""></div>
+
+                <div className=""></div>
+
+                <div className=""></div>
+
+                <div className=""></div>
 
             </div>
 
-            <div className=""></div>
-            
-            <div className=""></div>
-            
-            <div className=""></div>
-            
-            <div className=""></div>
-
         </div>
-
-    </div>
-  )
+    )
 }
