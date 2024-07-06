2feae5daedf3ea89f8fdcc7b21aa362e811a0df5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-06 10:39:14 +0200

    new code is added now

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import myIgm from '../Image/pexelsdivinetechygirl.jpg'
 import Image from 'next/image'
 import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 
 export default function Footer() {
@@ -14,15 +17,30 @@ export default function Footer() {
 
             <div className="max-w-screen-xl mx-auto">
 
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col">
                     <div className="w-24">
                         <Image src={ComponentWhatsApp} />
                     </div>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col text-slate-900 text-sm">
                         <p>We implement the corporation performance</p>
                         <p>Software TSk for consideration planning</p>
                         <p>And reporting purposes</p>
+                    </div>
+                    <div className="">
+                        <h1 className='text-xl font-bold text-slate-900'>Follow us :</h1>
+                        
+                        <div className="flex gap-3 items-center">
+                           <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                           <IoLogoInstagram className="" />
+                           </div>
+                           <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                          <FaFacebookF className="" />
+                          </div>
+                          <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                           <FaTwitter className="" />
+                           </div>
+                        </div>
                     </div>
                 </div>
 
