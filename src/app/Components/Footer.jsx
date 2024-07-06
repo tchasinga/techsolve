ff3ac24776015d875b6154f4b489bcd1d0c6bda0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-06 09:15:38 +0200

    new code is just added now

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="min-h-[500px]">
+    <div className="min-h-[500px]"
+    
+    >
+        
+        <div className="max-w-screen-xl mx-auto">
+            
+            <div className="">
+
+            </div>
+
+            <div className=""></div>
+            
+            <div className=""></div>
+            
+            <div className=""></div>
+            
+            <div className=""></div>
+
+        </div>
+
     </div>
   )
 }
