95d706f13e8b460a7ae0c56c26230d132f1fe191
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-06 09:50:12 +0200

    new code is just added now

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import myIgm from '../Image/pexelsdivinetechygirl.jpg'
+import Image from 'next/image'
+import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
+
 
 export default function Footer() {
     return (
         <div className="min-h-[500px]"
             style={{
-                background: `linear-gradient(rgba(4, 5, 5, 0.716), rgba(4, 5, 5, 0.716)), url(${myIgm.src}) center / cover no-repeat fixed`,
+                background: `linear-gradient(rgba(238, 238, 238, 0.7), rgba(238, 238, 238, 0.7)), url(${myIgm.src}) center / cover no-repeat fixed`,
             }}
         >
 
             <div className="max-w-screen-xl mx-auto">
 
-                <div className="">
+                <div className="flex flex-col justify-center items-center">
+                    <div className="w-24">
+                        <Image src={ComponentWhatsApp} />
+                    </div>
 
+                    <div className="flex flex-col">
+                        <p>We implement the corporation performance</p>
+                        <p>Software TSk for consideration planning</p>
+                        <p>And reporting purposes</p>
+                    </div>
                 </div>
 
+                {/* Second box */}
                 <div className=""></div>
 
+                {/* 3e box */}
+                <div className=""></div>
+                
+                {/* 4e box */}
                 <div className=""></div>
 
-                <div className=""></div>
-
+                {/* 5e box */}
                 <div className=""></div>
 
             </div>
