49f7ccab5fbb631a2160deae16b99a45b15d716d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-06 12:14:09 +0200

    new commit is just added now

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -6,6 +6,10 @@ import { IoLogoInstagram } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 
+import { FiPhoneCall } from "react-icons/fi";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+
 
 export default function Footer() {
     return (
@@ -15,7 +19,7 @@ export default function Footer() {
             }}
         >
 
-            <div className="max-w-screen-xl mx-auto">
+            <div className="max-w-screen-xl mx-auto flex justify-evenly items-center flex-wrap w-full min-h-[500px]">
 
                 <div className="flex flex-col">
                     <div className="w-24">
@@ -29,35 +33,96 @@ export default function Footer() {
                     </div>
                     <div className="">
                         <h1 className='text-xl font-bold text-slate-900'>Follow us :</h1>
-                        
+
                         <div className="flex gap-3 items-center">
-                           <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
-                           <IoLogoInstagram className="" />
-                           </div>
-                           <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
-                          <FaFacebookF className="" />
-                          </div>
-                          <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
-                           <FaTwitter className="" />
-                           </div>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                                <IoLogoInstagram className="" />
+                            </div>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                                <FaFacebookF className="" />
+                            </div>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-xl font-bold">
+                                <FaTwitter className="" />
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 {/* Second box */}
-                <div className=""></div>
+                <div className="flex flex-col py-2">
+                    <h1>Home</h1>
+                    <div className="">
+                        <p>Services</p>
+                        <p>About</p>
+                        <p>Blog</p>
+                        <p>Testimonials</p>
+                        <p>Contact</p>
+                    </div>
+                </div>
 
                 {/* 3e box */}
-                <div className=""></div>
-                
+                <div className="">
+                    <h1>Services</h1>
+                    <div className="">
+                        <p>Building website</p>
+                        <p>Building Apk & IOS</p>
+                        <p>Web application</p>
+                        <p>Digital product</p>
+                        <p>Etc...</p>
+                    </div>
+                </div>
+
                 {/* 4e box */}
-                <div className=""></div>
+                <div className="">
+                    <h1>Quick Links</h1>
+                    <div className="">
+                        <p>Terms & Condition</p>
+                        <p>Privacy Policy</p>
+                        <p>Help center</p>
+                        <p>Career</p>
+                        <p>Contact us</p>
+                    </div>
+                </div>
 
                 {/* 5e box */}
-                <div className=""></div>
+                <div className="flex-col flex justify-center">
+                    <h1>Contact us</h1>
+                    <div className="flex-col flex justify-center items-center">
+
+                        <div className='flex items-center justify-center gap-4'>
+                            <div>
+                                <FiPhoneCall />
+                            </div>
+                            <div>
+                                <h1>Call us : </h1>
+                                <p>(250) 78 72 890 28</p>
+                            </div>
+                        </div>
+
+                        <div className='flex items-center justify-center gap-4'>
+                            <div>
+                                <MdOutlineEmail />
+                            </div>
+                            <div>
+                                <h1>Email us : </h1>
+                                <p>tchasingajacques@gmail.com</p>
+                            </div>
+                        </div>
+
+                        <div className='flex items-center justify-center gap-4'>
+                            <div>
+                                <IoLocationOutline />
+                            </div>
+                            <div>
+                                <h1>Location : </h1>
+                                <p>KG 11 Ave 15, Kigali, Rwanda</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
-
         </div>
     )
 }
