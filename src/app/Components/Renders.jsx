15bf49f4407497ea3a54bcc5b8e08b4c936d7650
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-08 16:24:00 +0200

    new code is added now

--- src/app/Components/Renders.jsx
+++ src/app/Components/Renders.jsx
@@ -28,7 +28,7 @@ export default function Renders() {
     };
 
     return (
-        <div className="max-w-5xl mx-auto mt-[5%]">
+        <div className="max-w-5xl mx-auto mt-[5%] mytop">
             <motion.div 
                 initial="hidden"
                 animate="visible"
@@ -43,7 +43,7 @@ export default function Renders() {
                         >
                             <div className="w-[400px] border-2 border-red-400 m-3 backer rounded-lg relative" >
                                 <Image src={data.ImagesSide} className='mb-[-10px] mt-2 ml-3 rounded-lg' />
-                                <div className='absolute border-2 border-slate-900 top-[35%] bg-white p-9 rounded-full text-3xl left-[85%] '>
+                                <div className='absolute border-2 border-slate-900 top-[35%] bg-white p-9 rounded-full text-3xl left-[85%] reducer'>
                                     {data.MoreIcons}
                                 </div>
                             </div>
