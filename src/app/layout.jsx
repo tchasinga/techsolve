87ae1cfa1556465164d489cec5a5c6ac19bdb593
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-08 17:23:47 +0200

    new side update is added now

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "../app/globals.css"; // Ensure this path is correct based on your project structure
+import Head from "next/head";
+import "../app/globals.css"; 
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,6 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Techsol Software Kenya" />
+        <meta property="og:url" content="https://techsolvering.netlify.app/" />
+        <meta property="og:image" content="https://images.pexels.com/photos/3184430/pexels-photo-3184430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <meta property="og:description" content="A tech developer service offers expert guidance and solutions in software development, encompassing areas like web and mobile app creation, cloud computing, and cyber-security. These services typically include custom software development, system integration, and IT consulting to optimize" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
