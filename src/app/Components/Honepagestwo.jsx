6890f30713faee56ada3fab5169a6793ce06f1b5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-08 12:26:01 +0200

    new design....

--- src/app/Components/Honepagestwo.jsx
+++ src/app/Components/Honepagestwo.jsx
@@ -5,38 +5,37 @@ import CountUp from 'react-countup';
 import { motion } from "framer-motion"
 import { containerVariant } from '../Animation/animation.js';
 
-
 export default function HomepageTwo() {
   return (
-    <div class="max-w-screen-xl mx-auto">
-      <motion.div className="mx-auto pt-[100px] items-center justify-center flex gap-10 flex-wrap  w-full"
-      initial="offscreen"
-      whileInView={"onscreen"}
-      variants={containerVariant()}
+    <div className="max-w-screen-xl mx-auto p-4">
+      <motion.div 
+        className="mx-auto py-[50px] items-center justify-center flex gap-6 flex-wrap w-full"
+        initial="offscreen"
+        whileInView={"onscreen"}
+        variants={containerVariant()}
       >
-        <div className="text-black">
+        <div className="text-black text-center">
           <h1 className='text-2xl font-bold'>
             <CountUp end={500} suffix="k+ More" duration={15} />
           </h1>
           <p className='text-sm text-slate-900'>Trusted Companies Partner</p>
         </div>
 
-        <div className="text-black">
+        <div className="text-black text-center">
           <h1 className='text-2xl font-bold'>
             <CountUp end={300} suffix="k+ View" duration={15} />
           </h1>
           <p className='text-sm text-slate-900'>Partner of training journey</p>
         </div>
 
-        <div className="text-black">
+        <div className="text-black text-center">
           <h1 className='text-2xl font-bold'>
             <CountUp end={400} suffix="k+ View" duration={15} />
           </h1>
           <p className='text-sm text-slate-900'>Partner project designer</p>
         </div>
 
-
-        <div className="text-black">
+        <div className="text-black text-center">
           <h1 className='text-2xl font-bold'>
             <CountUp end={100} suffix="k+ View" duration={15} />
           </h1>
@@ -44,10 +43,11 @@ export default function HomepageTwo() {
         </div>
       </motion.div>
 
-      <motion.div className="min-h-screen w-full flex flex-col md:flex-row justify-between"
-      initial="offscreen"
-      whileInView={"onscreen"}
-      variants={containerVariant()}
+      <motion.div 
+        className="min-h-screen w-full flex flex-col md:flex-row justify-between items-center gap-6"
+        initial="offscreen"
+        whileInView={"onscreen"}
+        variants={containerVariant()}
       >
         <div className="w-full md:w-1/2 flex items-center justify-center">
           <video
@@ -62,18 +62,17 @@ export default function HomepageTwo() {
           />
         </div>
 
-        {/* Second div */}
         <div className="w-full md:w-1/2 flex flex-col justify-center p-4">
-          <h2 className="text-xl Advantages text-slate-900 font-bold mb-4 uppercase">Advantages of working with us</h2>
-          <div className="list-disc pl-4 text">
-            <div className="text-slate-900 text-base py-3 font-medium">
+          <h2 className="text-xl Advantages text-slate-900 font-bold mb-4 uppercase text-center md:text-left">Advantages of working with us</h2>
+          <div className="pl-4 text">
+            <div className="text-slate-900 text-base py-3 font-medium space-y-2">
               <p>Expertise and Experience: They bring extensive knowledge and skills.</p>
               <p>Customization and Scalability: Tailored solutions that grow with your business.</p>
               <p>Time Efficiency: Projects are completed faster with a dedicated team.</p>
               <p>Technical Support: Ongoing maintenance and support for your website.</p>
               <p>Access to Latest Technologies: Integration of modern technologies.</p>
             </div>
-            <div className="text-base font-medium text-slate-950">
+            <div className="text-base font-medium text-slate-950 space-y-2">
               <p>SEO and Marketing Integration: Enhance visibility and performance.</p>
               <p>Security: Implementation of robust security measures.</p>
               <p>Cost-Effectiveness: Save on hiring and infrastructure costs.</p>
@@ -83,7 +82,7 @@ export default function HomepageTwo() {
           </div>
         </div>
       </motion.div>
-      {/* Adding an Infinitie carousel */}
+      {/* Adding an Infinite carousel */}
     </div>
   )
 }
