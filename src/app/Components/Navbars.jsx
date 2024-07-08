64569f327d931d005f43cb0ec17d130dc48d198b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-08 17:14:13 +0200

    new code is added now

--- src/app/Components/Navbars.jsx
+++ src/app/Components/Navbars.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useState } from 'react'; // Removed unused useEffect import
 import '../globals.css'
+import Image from "next/image";
+import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
 
 export default function Navbars() {
     const [open, setOpen] = useState(false);
@@ -16,10 +18,8 @@ export default function Navbars() {
     return (
         <nav className='fixed font-poppins font-sans w-full left-0 top-0 z-[999] mypad'>
             <div className="flex items-center justify-between">
-                <div className="mx-7">
-                    <h4 className="text-sm myheader uppercase font-bold text-slate-200">
-                        Jet-codesign
-                    </h4>
+                <div className="mx-7 w-[150px]">
+                    <Image src={ComponentWhatsApp}/>
                 </div>
 
                 <div className='text-gray-900 md:block hidden px-7 py-2 font-medium'>
