674cebed70c7c95cd85715cc64330895cde19ea4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-09 09:46:23 +0200

    some commit for metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,17 +8,24 @@ const inter = Inter({
 export const metadata = {
   title: "Techsol Software Kenya",
   description: "A tech developer service offers expert guidance and solutions in software development, encompassing areas like web and mobile app creation, cloud computing, and cyber-security. These services typically include custom software development, system integration, and IT consulting to optimize",
+  openGraph: {
+    type: "website",
+    title: "Techsol Software Kenya",
+    url: "https://techsolvering.netlify.app/",
+    image: "https://images.pexels.com/photos/3184430/pexels-photo-3184430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description: "A tech developer service offers expert guidance and solutions in software development, encompassing areas like web and mobile app creation, cloud computing, and cyber-security. These services typically include custom software development, system integration, and IT consulting to optimize"
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Techsol Software Kenya" />
-        <meta property="og:url" content="https://techsolvering.netlify.app/" />
-        <meta property="og:image" content="https://images.pexels.com/photos/3184430/pexels-photo-3184430.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <meta property="og:description" content="A tech developer service offers expert guidance and solutions in software development, encompassing areas like web and mobile app creation, cloud computing, and cyber-security. These services typically include custom software development, system integration, and IT consulting to optimize" />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:image" content={metadata.openGraph.image} />
+        <meta property="og:description" content={metadata.openGraph.description} />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
