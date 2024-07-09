f50e804afec78751df1bbba15949ee54489e8bcb
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-09 13:50:16 +0200

    my closerLaster updater

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,53 +2,45 @@ import React from 'react'
 import myIgm from '../Image/pexelsdivinetechygirl.jpg'
 import Image from 'next/image'
 import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
-import { IoLogoInstagram } from "react-icons/io5";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-
+import { IoLogoInstagram, IoLocationOutline } from "react-icons/io5";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { FiPhoneCall } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
-import { IoLocationOutline } from "react-icons/io5";
-
+import { SiWhatsapp } from "react-icons/si";
 
 export default function Footer() {
     return (
-        <div className="min-h-[500px]" id='contact'
+        <div className="min-h-[500px] bg-cover bg-fixed" id='contact'
             style={{
                 background: `linear-gradient(rgba(238, 238, 238, 0.7), rgba(238, 238, 238, 0.7)), url(${myIgm.src}) center / cover no-repeat fixed`,
             }}
         >
-
-            <div className="max-w-screen-xl mx-auto flex justify-evenly align-middle items-center flex-wrap w-full min-h-[500px] py-[50px]">
-
-                <div className="flex flex-col ">
+            <div className="max-w-screen-xl mx-auto flex justify-evenly items-center flex-wrap w-full min-h-[500px] py-12">
+                <div className="flex flex-col items-center">
                     <div className="w-24">
                         <Image src={ComponentWhatsApp} />
                     </div>
-
-                    <div className="flex flex-col text-slate-900 text-sm py-2">
+                    <div className="flex flex-col text-slate-900 text-sm py-2 text-center">
                         <p>We implement the corporation performance</p>
                         <p>Software TSk for consideration planning</p>
                         <p>And reporting purposes</p>
                     </div>
                     <div className="">
-                        <h1 className='text-lg font-bold text-slate-900'>Follow us :</h1>
-
+                        <h1 className='text-lg font-bold text-slate-900'>Follow us:</h1>
                         <div className="flex gap-3 items-center py-3">
-                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
-                                <IoLogoInstagram className="" />
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
+                                <IoLogoInstagram />
                             </div>
-                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
-                                <FaFacebookF className="" />
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
+                                <FaFacebookF />
                             </div>
-                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
-                                <FaTwitter className="" />
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
+                                <FaTwitter />
                             </div>
                         </div>
                     </div>
                 </div>
 
-                {/* Second box */}
                 <div className="flex flex-col py-2">
                     <h1 className='text-lg font-bold text-slate-900'>Home</h1>
                     <div className="flex flex-col text-slate-900 text-sm py-2">
@@ -60,8 +52,7 @@ export default function Footer() {
                     </div>
                 </div>
 
-                {/* 3e box */}
-                <div className="">
+                <div className="flex flex-col py-2">
                     <h1 className='text-lg font-bold text-slate-900'>Services</h1>
                     <div className="flex flex-col text-slate-900 text-sm py-2">
                         <p>Building website</p>
@@ -72,8 +63,7 @@ export default function Footer() {
                     </div>
                 </div>
 
-                {/* 4e box */}
-                <div className="">
+                <div className="flex flex-col py-2">
                     <h1 className='text-lg font-bold text-slate-900'>Quick Links</h1>
                     <div className="flex flex-col text-slate-900 text-sm py-2">
                         <p>Terms & Condition</p>
@@ -84,43 +74,50 @@ export default function Footer() {
                     </div>
                 </div>
 
-                {/* 5e box */}
-                <div className="flex-col flex justify-center items-center">
+                <div className="flex flex-col justify-center items-center">
                     <h1 className='text-lg font-bold text-slate-900'>Contact us</h1>
-                    <div className="flex-col flex justify-center items-center">
-                    <div className='flex items-center justify-center  gap-4'>
-                        <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
+                    <div className="">
+                        <div className='flex items-center gap-4 py-1'>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
                                 <FiPhoneCall />
                             </div>
                             <div>
-                                <h1>Call us : </h1>
-                                <p>(250)787289028 call this number</p>
+                                <h1>Call us:</h1>
+                                <p>(250) 787-289-028</p>
                             </div>
                         </div>
-
-                        <div className='flex justify-center items-center gap-4'>
-                        <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
+                        <div className='flex items-center gap-4 py-1'>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
                                 <MdOutlineEmail />
                             </div>
                             <div>
-                                <h1>Email us : </h1>
+                                <h1>Email us:</h1>
+                                <div className="text-slate-700 text-sm font-medium">
                                 <p>tchasingajacques@gmail.com</p>
+                                <p>Kiokoj81@gmail.com</p>
+                                </div>
                             </div>
                         </div>
-
-                        <div className='flex items-center justify-center gap-4'>
-                        <div className="bg-blue-600 p-3 rounded-full text-white text-lg font-bold">
+                        <div className='flex items-center gap-4 py-1'>
+                            <div className="bg-blue-600 p-3 rounded-full text-white text-lg">
                                 <IoLocationOutline />
                             </div>
                             <div>
-                                <h1>Location : </h1>
+                                <h1>Location:</h1>
                                 <p>KG 11 Ave 15, Kigali, Rwanda</p>
                             </div>
                         </div>
-
+                        <div className='flex items-center gap-4 py-2'>
+                            <div className="bg-green-500 p-3 rounded-full text-white text-lg">
+                                <SiWhatsapp />
+                            </div>
+                            <div>
+                                <h1>WhatsApp:</h1>
+                                <p>+254 710-420-745</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     )
