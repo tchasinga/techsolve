c0a84a5b7ff5397d1d7479c76a3f2d52c2653f5c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-10 08:18:35 +0200

    new code is added now

--- src/app/Animation/animation.js
+++ src/app/Animation/animation.js
@@ -1,107 +1,100 @@
-
 // this will  be my animation file using framer motion
 export const containerVariant = (delay = 0.5) => ({
-    offscreen: {
-        opacity: -0,
-        y: 50,
+  offscreen: {
+    opacity: -0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 10,
+    y: 0,
+    transition: {
+      delay: delay,
+      duration: 2.5,
+      type: "spring",
     },
-    onscreen: {
-        opacity: 10,
-        y: 0,
-        transition: {
-            delay: delay,
-            duration: 2.5,
-            type: "spring",
-        }
-    }
-})
+  },
+});
 
 export const textVariantReveals = (delay = 1) => ({
-    offscreen: {
-        opacity: 0,
-        y: 0,
+  offscreen: {
+    opacity: 0,
+    y: 0,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: delay,
+      duration: 3.5,
+      type: "spring",
+      stiffness: 100,
+      damping: 20,
     },
-    onscreen: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            delay: delay,
-            duration: 3.5,
-            type: "spring",
-            stiffness: 100,
-            damping: 20,
-        }
-    }
+  },
 });
-
 
 export const textVariantRevealser = (delay = 1) => ({
-    offscreen: {
-        opacity: 0,
-        y: 0,
+  offscreen: {
+    opacity: 0,
+    y: 0,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 20,
+    transition: {
+      delay: delay,
+      duration: 2.5,
+      type: "spring",
+      stiffness: 100,
+      damping: 20,
     },
-    onscreen: {
-        opacity: 1,
-        y: 20,
-        transition: {
-            delay: delay,
-            duration: 2.5,
-            type: "spring",
-            stiffness: 100,
-            damping: 20,
-        }
-    }
+  },
 });
 
-
 export const ImageOne = (delay = 0.5) => ({
-    offscreen: {
-        opacity: 0,
-        y: 50,
+  offscreen: {
+    opacity: 0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 4,
+    y: 0,
+    transition: {
+      delay: delay,
+      duration: 2.5,
+      type: "spring",
     },
-    onscreen: {
-        opacity: 4,
-        y: 0,
-        transition: {
-            delay: delay,
-            duration: 2.5,
-            type: "spring",
-        }
-    }
-})
+  },
+});
 
 export const ImageTwo = (delay = 1) => ({
-    offscreen: {
-        opacity: 0,
-        y: 50,
+  offscreen: {
+    opacity: 0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 4,
+    y: 0,
+    transition: {
+      delay: delay,
+      duration: 2.5,
+      type: "spring",
     },
-    onscreen: {
-        opacity: 4,
-        y: 0,
-        transition: {
-            delay: delay,
-            duration: 2.5,
-            type: "spring",
-        }
-    }
-})
-
+  },
+});
 
 export const ImageThree = (delay = 1.5) => ({
-    offscreen: {
-        opacity: 0,
-        y: 50,
+  offscreen: {
+    opacity: 0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 4,
+    y: 0,
+    transition: {
+      delay: delay,
+      duration: 2.5,
+      type: "spring",
     },
-    onscreen: {
-        opacity: 4,
-        y: 0,
-        transition: {
-            delay: delay,
-            duration: 2.5,
-            type: "spring",
-        }
-    }
-})
-
-
-
+  },
+});
