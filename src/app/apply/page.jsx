695fd96a47ac21925a7ea012fe4b07357bb510cd
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-16 09:21:08 +0200

    new code code is added now

--- src/app/apply/page.jsx
+++ src/app/apply/page.jsx
@@ -1,10 +1,26 @@
 'use client'
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-export default function Applyingpages() {
+export default function MultilineTextFields() {
   return (
-    <div>
-       this is my pages now 
-    </div>
-  )
+    <div className='max-w-4xl mx-auto flex flex-col justify-center min-h-screen '>
+      
+        <TextField
+          id=""
+          label="Multiline is me "
+          multiline
+          
+        />
+       
+        <TextField
+          id=""
+          label="Multiline"
+          multiline
+          rows={10}
+        />
+   </div>
+  );
 }
+
