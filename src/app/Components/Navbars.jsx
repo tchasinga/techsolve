d5e78a308cd2a24fa6c7d8ca470e4a820b764767
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-16 08:54:40 +0200

    new code is adde now

--- src/app/Components/Navbars.jsx
+++ src/app/Components/Navbars.jsx
@@ -1,84 +1,90 @@
-'use client';
-import React from 'react';
-import { useState } from 'react'; // Removed unused useEffect import
-import '../globals.css'
+"use client";
+import React from "react";
+import { useState } from "react"; 
+import "../globals.css";
 import Image from "next/image";
-import ComponentWhatsApp from '../Log/WhatsApp.jpeg'
-import Link from 'next/link';
+import ComponentWhatsApp from "../Log/WhatsApp.jpeg";
+import Link from "next/link";
 
 export default function Navbars() {
-    const [open, setOpen] = useState(false);
-    const menuLinks = [
-        { name: "Welcom", link: "#home" },
-        { name: "Advantage", link: "#advantage" },
-        { name: "Description", link: "#description" },
-        { name: "Services", link: "#services" },
-        { name: "Our-works", link: "#theworks" },
-        {name : "Apply-now", link : '../apply'},
-        { name: "Contact", link: "#contact" },
-        
-    ];
+  const [open, setOpen] = useState(false);
+  const menuLinks = [
+    { name: "Welcom", link: "#home" },
+    { name: "Advantage", link: "#advantage" },
+    { name: "Description", link: "#description" },
+    { name: "Services", link: "#services" },
+    { name: "Our-works", link: "#theworks" },
+    { name: "Apply-now", link: "../apply" },
+    { name: "Contact", link: "#contact" },
+  ];
 
-    return (
-        <nav className='fixed font-poppins font-sans w-full left-0 top-0 z-[999] mypad bg-blacking'>
-            <div className="flex items-center justify-between">
-                <div className="mx-7 w-[150px]">
-                    <Image src={ComponentWhatsApp}/>
-                </div>
+  return (
+    <nav className="fixed font-poppins font-sans w-full left-0 top-0 z-[999] mypad bg-blacking">
+      <div className="flex items-center justify-between">
+        <div className="mx-7 w-[150px]">
+          <Image src={ComponentWhatsApp} />
+        </div>
 
-                <div className='text-gray-900 md:block hidden px-7 py-2 font-medium'>
-                    <ul className="flex items-center font-poppins font-sans py-2 text-sm">
-                        {menuLinks.map((menu, i) => (
-                            <li key={i} className="px-3 text-white font-poppins font-sans hover:text-cyan-600">
-                                <Link href={menu.link}>{menu.name}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div
-                    onClick={() => setOpen(!open)}
-                    className={`z-[999] ${open ? "text-gray-200" : "text-gray-100"} text-3xl md:hidden m-5`}
-                >
-                    <svg
-                        className="w-8 h-8 text-slate-200 "
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        {open ? (
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M6 18L18 6M6 6l12 12"
-                            ></path>
-                        ) : (
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            ></path>
-                        )}
-                    </svg>
-                </div>
-                <div
-                    className={`md:hidden text-gray-900 absolute w-full h-screen px-7 py-2 font-medium bg-black top-0 duration-300 ${open ? "right-0" : "right-[-100%]"}`}
-                >
-                    <ul className="flex flex-col justify-center h-full text-white gap-10 py-2 text-lg">
-                        {menuLinks.map((menu, i) => (
-                            <li
-                                onClick={() => setOpen(false)}
-                                key={i}
-                                className="px-6 hover:text-cyan-600"
-                            >
-                                <Link href={menu.link}>{menu.name}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="text-gray-900 md:block hidden px-7 py-2 font-medium">
+          <ul className="flex items-center font-poppins font-sans py-2 text-sm">
+            {menuLinks.map((menu, i) => (
+              <li
+                key={i}
+                className="px-3 text-white font-poppins font-sans hover:text-cyan-600"
+              >
+                <Link href={menu.link}>{menu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`z-[999] ${
+            open ? "text-gray-200" : "text-gray-100"
+          } text-3xl md:hidden m-5`}
+        >
+          <svg
+            className="w-8 h-8 text-slate-200 "
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {open ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              ></path>
+            )}
+          </svg>
+        </div>
+        <div
+          className={`md:hidden text-gray-900 absolute w-full h-screen px-7 py-2 font-medium bg-black top-0 duration-300 ${
+            open ? "right-0" : "right-[-100%]"
+          }`}
+        >
+          <ul className="flex flex-col justify-center h-full text-white gap-10 py-2 text-lg">
+            {menuLinks.map((menu, i) => (
+              <li
+                onClick={() => setOpen(false)}
+                key={i}
+                className="px-6 hover:text-cyan-600"
+              >
+                <Link href={menu.link}>{menu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
