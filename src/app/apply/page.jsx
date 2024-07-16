ea8ec2bdfb53d1e9e66e22215744131a038190da
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-16 09:32:09 +0200

    new code is added now

--- src/app/apply/page.jsx
+++ src/app/apply/page.jsx
@@ -1,26 +1,16 @@
-'use client'
-import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+"use client";
+import React from "react";
+import TextField from "@mui/material/TextField";
+import { Autocomplete } from "@mui/material";
+
 
 export default function MultilineTextFields() {
+  const skill = ['Java' , 'Html' , 'JavaScript' , 'MongoDb' , 'React' , 'Mysql']
   return (
-    <div className='max-w-4xl mx-auto flex flex-col justify-center min-h-screen '>
-      
-        <TextField
-          id=""
-          label="Multiline is me "
-          multiline
-          
-        />
-       
-        <TextField
-          id=""
-          label="Multiline"
-          multiline
-          rows={10}
-        />
-   </div>
+    <div className="max-w-4xl mx-auto flex flex-col justify-center min-h-screen ">
+      <TextField id="" label="Multiline is me " multiline />
+      <TextField id="" label="Multiline" multiline rows={10} />
+      <Autocomplete  options={skill} placement='bottom' renderInput={(params) => <TextField  {...params} label="Services"/>}/>
+    </div>
   );
 }
-
