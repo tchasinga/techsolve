54e7164ad31d55daa854059d4e83b08184aa2679
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-19 14:39:28 +0200

    new code

--- src/app/apply/page.jsx
+++ src/app/apply/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
-import { Autocomplete, Button } from "@mui/material";
+import { Button } from "@mui/material";
 import {
   getDownloadURL,
   getStorage,
